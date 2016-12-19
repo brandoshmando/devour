a85083fc9d9a8af9a3766f7def43616993fd2468
Brando <brandon@getambassador.com>
2016-12-19 17:48:46 -0500

    More error handling updates. Separate utils into different files

--- devour/utils/loaders.py
+++ devour/utils/loaders.py
@@ -2,20 +2,7 @@ import sys, os
 from contextlib import contextmanager
 
 from devour import exceptions
-
-@contextmanager
-def cwd():
-    # get og state on enter
-    in_path = os.getcwd() in sys.path
-    if not in_path:
-        sys.path.insert(0, os.getcwd())
-    yield
-    # if og state not in path, remove
-    if not in_path:
-        try:
-            sys.path.remove(os.getcwd())
-        except ValueError:
-            pass
+from .helpers import cwd
 
 def load_module(path):
     with cwd():
@@ -38,6 +25,6 @@ def load_consumer_class(path):
         try:
             cls = getattr(mod, cls_name)
         except AttributeError:
-            pass
+            raise exceptions.DevourConfigException('consumer class %s is not defined' % cls_name)
 
         return cls
